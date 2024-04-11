b70fd2f82a6c796903770fe91b62e6d0b74f5a1c
moxn <moxn.jj@gmail.com>
2024-04-11 16:00:50 +0900

    userCreateController 메소드 테스트 코드 1차 완료

--- test-code-with-architecture/src/test/java/com/example/demo/controller/UserCreateControllerTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -1,29 +1,64 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc // MockMVC를 사용하기 위한 어노테이션
-@AutoConfigureTestDatabase // MockMVC를 사용하기 위한 어노테이션
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class UserCreateControllerTest {
 
     // MockMVC : controller 테스트를 위해 사용하며, api 테스트할 때 많이 사용됨
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
-    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
-        // 우리 서버에 mockMvc를 통해 api를 get방식으로 호출한다.
-        mockMvc.perform(get("/health_check.html"))
-                .andExpect(status().isOk());
+    void 사용자는_회원_가입을_할_수_있고_회원_가입된_사용자는_PENDING_상태이다() throws Exception {
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("moxn.jj@gmail.com")
+                .nickname("moxn.jj")
+                .address("Pangyo")
+                .build();
+        BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        // then
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value("moxn.jj@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("moxn.jj"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
     }
 }
