3a7c9c635a58386201612f9063851a502e9a40a8
moxn <moxn.jj@gmail.com>
2024-04-17 11:39:11 +0900

    Test: post 관련 컨트롤러 테스트 케이스 추가

--- test-code-with-architecture/src/test/java/com/example/demo/post/controller/PostControllerTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/post/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package com.example.demo.post.controller;
 
 
 import com.example.demo.post.domain.PostCreate;
+import com.example.demo.post.domain.PostUpdate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +14,15 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @SqlGroup({
-        @Sql(value = "/sql/user-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+        @Sql(value = "/sql/post-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
         , @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
 public class PostControllerTest {
@@ -32,23 +33,46 @@ public class PostControllerTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void 사용자는_게시물을_생성할_수_있다() throws Exception {
+    void 사용자는_게시물을_단건_조회할_수_있다() throws Exception {
         // given
-        PostCreate postCreate = PostCreate.builder()
-                .writerId(1)
-                .content("hello world")
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("helloworld"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("moxn.jj@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("moxn.jj"));
+    }
+
+    @Test
+    void 사용자는_존재하지_않는_게시물을_조회_시_에러가_난다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/posts/987654321124"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Posts에서 ID 987654321124를 찾을 수 없습니다."));
+    }
+    @Test
+    void 사용자는_게시물을_수정할_수_있다() throws Exception {
+        // given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("hello world!")
                 .build();
 
         // when
         // then
-        mockMvc.perform(post("/api/posts")
+        mockMvc.perform(put("/api/posts/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(postCreate)))
-                .andExpect(status().isCreated())
+                        .content(objectMapper.writeValueAsBytes(postUpdate)))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").isNumber())
-                .andExpect(jsonPath("$.content").value("hello world"))
-                // .andExpect(jsonPath("$.createdAt").value(""))
-        ;
+                .andExpect(jsonPath("$.content").value("hello world!"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("moxn.jj@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("moxn.jj"));
     }
 
 }
--- test-code-with-architecture/src/test/java/com/example/demo/post/controller/PostCreateControllerTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/post/controller/PostCreateControllerTest.java
@@ -1,30 +1,57 @@
 package com.example.demo.post.controller;
 
 
+import com.example.demo.post.domain.PostCreate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc // MockMVC를 사용하기 위한 어노테이션
-@AutoConfigureTestDatabase // MockMVC를 사용하기 위한 어노테이션
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+        , @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+
+})
 public class PostCreateControllerTest {
 
     // MockMVC : controller 테스트를 위해 사용하며, api 테스트할 때 많이 사용됨
     @Autowired
     private MockMvc mockMvc;
 
-    // TODO : USER와 같이 채워보기
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
-    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
-        // 우리 서버에 mockMvc를 통해 api를 get방식으로 호출한다.
-        mockMvc.perform(get("/health_check.html"))
-                .andExpect(status().isOk());
+    void 사용자는_게시물을_생성할_수_있다() throws Exception {
+        // given
+        PostCreate postCreate = PostCreate.builder()
+                .writerId(1)
+                .content("helloworld")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(post("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(postCreate)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("helloworld"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("moxn.jj@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("moxn.jj"));
     }
 }
