157c1c63aecc5915d3019750bc06dbe4218de8a7
mun <mun.xml@gmail.com>
2024-05-11 17:15:29 +0900

    CertificationServiceTest 테스트 추가

---
+++ test-code-with-architecture/src/test/java/com/example/demo/user/mock/FakeMailSender.java
@@ -0,0 +1,22 @@
+package com.example.demo.user.mock;
+
+import com.example.demo.user.service.port.MailSender;
+
+/**
+ * 테스트에서 메일 발송을 대체할 페이크 객체
+ */
+public class FakeMailSender implements MailSender {
+
+    // 실제로 어떤 값이 들어왔는 지 확인할 수 있도록 멤버변수 만들어 줌
+    public String email;
+    public String title;
+    public String content;
+
+
+    @Override
+    public void send(String email, String title, String content) {
+        this.email = email;
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ test-code-with-architecture/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+import com.example.demo.user.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_콘텐츠가_제대로_만들어져서_보내지는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("moxn.jj@gmail.com", 1, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("moxn.jj@gmail.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+    }
+}
